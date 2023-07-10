2fb8068ceeacfa0cc7e95e29baf4d0eeb6a9a9b0
behemoth-namph <namph@behemoth.vn>
2023-07-11 03:29:21 +0700

    account manager page

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,3 +11,30 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+function collapsed() {
+  const element = document.getElementById("collapsed");
+  if (element.classList.contains("collapsed")) {
+    element.classList.remove("collapsed");
+  } else {
+    element.classList.add("collapsed");
+  }
+}
+
+function toggleAccountBalance() {
+  const element = document.getElementById("account-balance");
+  if (element.classList.contains("show")) {
+    element.classList.remove("show");
+  } else {
+    element.classList.add("show");
+  }
+}
+
+function showAllFeature() {
+  const element = document.getElementById("main-content");
+  if (element.classList.contains("show-all")) {
+    element.classList.remove("show-all");
+  } else {
+    element.classList.add("show-all");
+  }
+}
