e01f4a7a651cc7dc7ed03fc94709a1df1d144b14
behemoth-namph <namph@behemoth.vn>
2023-07-27 04:58:06 +0700

    add popup notification

--- assets/js/app.js
+++ assets/js/app.js
@@ -47,3 +47,50 @@ function showLimit() {
     element.classList.add("d-block");
   }
 }
+
+function toast({ title = "", message = "", type = "info", duration = 3000 }) {
+  const main = document.getElementById("toast");
+
+  if (main) {
+    const toast = document.createElement("div");
+
+    // Auto remove toast
+    const autoRemoveId = setTimeout(function () {
+      main.removeChild(toast);
+    }, duration + 1000);
+
+    // Remove toast when clicked
+    toast.onclick = function (e) {
+      if (e.target.closest(".toast__close")) {
+        main.removeChild(toast);
+        clearTimeout(autoRemoveId);
+      }
+    };
+
+    const icons = {
+      success: "assets/icons/success.svg",
+      info: "assets/icons/info.svg",
+      warning: "assets/icons/warning.svg",
+      error: "assets/icons/error.svg",
+    };
+    const icon = icons[type];
+    const delay = (duration / 1000).toFixed(2);
+
+    toast.classList.add("toast", `toast--${type}`);
+    toast.style.animation = `slideInLeft ease .3s, fadeOut linear 1s ${delay}s forwards`;
+
+    toast.innerHTML = `
+              <div class="toast__icon">
+                <img src="${icon}" alt="success" />
+              </div>
+              <div class="toast__body">
+                  <h3 class="toast__title">${title}</h3>
+                  <p class="toast__msg">${message}</p>
+              </div>
+              <div class="toast__close">
+                  <i class="isax isax-close-circle"> </i>
+              </div>
+          `;
+    main.appendChild(toast);
+  }
+}
